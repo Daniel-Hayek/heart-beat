e36d50e0aa9a5bb8da184694df8bc3c80de47278
Danny <dhayek92@gmail.com>
2025-08-22 18:05:00 +0300

    FE: Created custom button widget

--- heart_beat_client/lib/widgets/primary_button.dart
+++ heart_beat_client/lib/widgets/primary_button.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:heart_beat_client/core/app_colors.dart';
 
 class PrimaryButton extends StatelessWidget {
-const PrimaryButton({ Key? key }) : super(key: key);
+  final String label;
+  final VoidCallback onPressed;
+
+  const PrimaryButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+  });
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        backgroundColor: AppColors.backgroundColor,
+      ),
+      child: Text(label, style: const TextStyle(fontSize: 16, color: AppColors.black)),
+    );
   }
 }
