c6325351e51f4b581bc220d35147d2f0f5a6703b
Danny <dhayek92@gmail.com>
2025-08-23 15:01:21 +0300

    FE: Created secondary button widget

---
+++ heart_beat_client/lib/widgets/secondary_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:heart_beat_client/core/app_colors.dart';
+
+class SecondaryButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const SecondaryButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: OutlinedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        backgroundColor: null,
+        side: const BorderSide(color: AppColors.primaryColor, width: 1.5),
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(fontSize: 16, color: AppColors.primaryColor),
+      ),
+    );
+  }
+}
