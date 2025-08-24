94a9eeb62da9dac53021952f74d6f2b1e7ddaccc
Danny <dhayek92@gmail.com>
2025-08-24 22:50:13 +0300

    BE: Generated resource for user table

---
+++ heart_beat_server/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { User } from './src/users/user.entity'; // adjust path
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '12345',
+  database: 'heartbeat_db',
+  entities: [User],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+});
