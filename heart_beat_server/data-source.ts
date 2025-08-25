c1944e98c9fd49e295ae987f76eae944270bf0e1
Danny <dhayek92@gmail.com>
2025-08-25 10:28:57 +0300

    BE: Created general folder strucutre to be used for entities and modules and created user entity

--- heart_beat_server/data-source.ts
+++ heart_beat_server/data-source.ts
@@ -1,5 +1,5 @@
 import { DataSource } from 'typeorm';
-import { User } from './src/models/users/entities/user.entity';
+import { User } from './src/entities/user.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
