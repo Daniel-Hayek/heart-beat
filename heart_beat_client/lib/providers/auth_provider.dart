9dfe6158ab69d5aeb49a5c2a60ec9e0a8432d5be
Danny <dhayek92@gmail.com>
2025-08-26 21:15:20 +0300

    FE: Added flutter secure storage to save token to persist between app restarts

--- heart_beat_client/lib/providers/auth_provider.dart
+++ heart_beat_client/lib/providers/auth_provider.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthProvider extends ChangeNotifier {
+  final _storage = const FlutterSecureStorage();
+
   String? _token;
   String? _userEmail;
 
   String? get token => _token;
   bool get isLoggedIn => _token != null;
 
-  void login (String token, String email) {
-    _token = token;
-    _userEmail = email;
+  Future<void> _loadToken() async {
+    _token = await _storage.read(key: 'auth_token');
+
     notifyListeners();
   }
 
-  void logout () {
+  void login(String token) async {
+    _token = token;
+    //_userEmail = email;
+    await _storage.write(key: 'auth_token', value: token);
+
+    notifyListeners();
+  }
+
+  void logout() async {
     _token = null;
-    _userEmail = null;
+
+    await _storage.delete(key: 'auth_token');
+    //_userEmail = null;
     notifyListeners();
   }
 }
