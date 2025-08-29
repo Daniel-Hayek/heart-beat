70dfd540f9cbdc3c3641544424d05269e50c00c5
Danny <dhayek92@gmail.com>
2025-08-29 14:19:53 +0300

    FE: Added jwt decoder and added ability to fetch token from secure storage

--- heart_beat_client/lib/screens/home/home_screen.dart
+++ heart_beat_client/lib/screens/home/home_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:heart_beat_client/providers/auth_provider.dart';
+import 'package:heart_beat_client/widgets/auth/auth_snack_bar.dart';
 import 'package:heart_beat_client/widgets/common/custom_app_bar.dart';
 import 'package:heart_beat_client/widgets/common/small_logo.dart';
 import 'package:heart_beat_client/widgets/home/home_info_card.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,7 +14,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context, listen: false);
     return Scaffold(
       appBar: CustomAppBar(title: "Home Screen"),
       body: Column(
@@ -18,6 +21,17 @@ class HomeScreen extends StatelessWidget {
           SmallLogo(),
           Text("How are you doing today, User?"),
           Column(children: [HomeInfoCard()]),
+          ElevatedButton(
+            onPressed: () async {
+              final authProvider = context.read<AuthProvider>();
+              final token = JwtDecoder.decode(authProvider.token!);
+
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(AuthSnackBar(content: Text(token.toString())));
+            },
+            child: const Text("Get token"),
+          ),
         ],
       ),
       bottomNavigationBar: BottomAppBar(),
