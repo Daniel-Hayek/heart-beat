db0aa8c30a018b35b5d629ab458a49134c34ef98
Danny <dhayek92@gmail.com>
2025-08-29 18:49:08 +0300

    FE: Added custom icons for bottom nav bar

--- heart_beat_client/lib/widgets/common/custom_bottom_bar.dart
+++ heart_beat_client/lib/widgets/common/custom_bottom_bar.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heart_beat_client/core/constants/app_colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomBottomBar extends StatelessWidget implements PreferredSizeWidget {
   final String currentActive;
@@ -11,14 +11,52 @@ class CustomBottomBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       items: [
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.graph_square), label: 'Stats'),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.book), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.music_note_2), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.chat_bubble), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/graph.svg',
+            width: 30,
+            height: 30,
+          ),
+          label: "Graph",
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/journal.svg',
+            width: 30,
+            height: 30,
+          ),
+          label: "Journal",
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/home.svg',
+            width: 30,
+            height: 30,
+          ),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/music.svg',
+            width: 30,
+            height: 30,
+          ),
+          label: "Music",
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/chatbot.svg',
+            width: 30,
+            height: 30,
+          ),
+          label: "Chatbot",
+        ),
       ],
       backgroundColor: AppColors.primaryColor,
       type: BottomNavigationBarType.fixed,
+      currentIndex: 1,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
     );
   }
 
