c90862a5fadfe747975f52e372c3d26fe5c723f8
Danny <dhayek92@gmail.com>
2025-08-31 11:50:09 +0300

    FE: Cleaned up some code that was throwing errors in the journal pages

--- heart_beat_client/lib/routes/app_routes.dart
+++ heart_beat_client/lib/routes/app_routes.dart
@@ -41,5 +41,6 @@ class AppRoutes {
           ),
         );
     }
+    return null;
   }
 }
--- heart_beat_client/lib/screens/journal/view_journal_screen.dart
+++ heart_beat_client/lib/screens/journal/view_journal_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heart_beat_client/core/constants/app_colors.dart';
-import 'package:heart_beat_client/core/constants/sample_text.dart';
-import 'package:heart_beat_client/widgets/common/fonts/body_text.dart';
 import 'package:heart_beat_client/widgets/common/fonts/title_text.dart';
 
 class ViewJournalScreen extends StatelessWidget {
