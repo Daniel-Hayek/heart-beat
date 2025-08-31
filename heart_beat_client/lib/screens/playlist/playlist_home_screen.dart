6743a993a576f19184a8386a9c23c040685dc7d3
Danny <dhayek92@gmail.com>
2025-08-31 13:45:16 +0300

    FE: Created basic layout for playlist page

--- heart_beat_client/lib/screens/playlist/playlist_home_screen.dart
+++ heart_beat_client/lib/screens/playlist/playlist_home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:heart_beat_client/core/constants/app_colors.dart';
 import 'package:heart_beat_client/widgets/common/bars/custom_app_bar.dart';
 import 'package:heart_beat_client/widgets/common/bars/custom_bottom_bar.dart';
+import 'package:heart_beat_client/widgets/common/fonts/title_text.dart';
 
 class PlaylistHomeScreen extends StatelessWidget {
   const PlaylistHomeScreen({super.key});
@@ -9,7 +12,24 @@ class PlaylistHomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Playlists"),
-      body: Column(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 4),
+            child: TitleText(
+              text: "What do you feel like listening to today?",
+              size: 20,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColors.primaryColor,
+        shape: CircleBorder(),
+        child: Icon(CupertinoIcons.add),
+      ),
       bottomNavigationBar: CustomBottomBar(),
     );
   }
