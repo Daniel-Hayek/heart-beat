4d1c961c237179e7647088fb53fee7c8bd36576e
Danny <dhayek92@gmail.com>
2025-09-01 11:50:23 +0300

    FE: Added styling and layout to quiz page

--- heart_beat_client/lib/screens/quiz/quiz_base_screen.dart
+++ heart_beat_client/lib/screens/quiz/quiz_base_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:heart_beat_client/core/constants/app_colors.dart';
 import 'package:heart_beat_client/widgets/common/bars/custom_app_bar.dart';
 import 'package:heart_beat_client/widgets/common/bars/side_bar.dart';
 import 'package:heart_beat_client/widgets/common/buttons/primary_button.dart';
@@ -18,29 +19,38 @@ class QuizBaseScreen extends StatelessWidget {
         children: [
           Container(
             padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 4),
+            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 4),
             child: TitleText(
               text: "Let's figure out how you're feeling!",
               size: 20,
             ),
           ),
+          SizedBox(height: 60),
           Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
             margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            height: 200,
+            decoration: BoxDecoration(
+              color: AppColors.secondaryColor,
+              borderRadius: BorderRadius.circular(8),
+            ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TitleText(text: "Last Mood Quiz Result", size: 20),
                 TitleText(text: "mm/yy/dd", size: 19),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TitleText(text: "Sad", size: 30),
-                    Icon(LucideIcons.frown),
+                    Icon(LucideIcons.frown, size: 90, color: Colors.white),
                   ],
                 ),
-                PrimaryButton(onPressed: () {}, label: "Take A Quiz"),
               ],
             ),
           ),
+          PrimaryButton(onPressed: () {}, label: "Take A Quiz"),
         ],
       ),
     );
