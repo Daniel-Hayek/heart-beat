79246d4cfffa80e913ae97b128776e01d4696051
Danny <dhayek92@gmail.com>
2025-09-01 17:17:28 +0300

    FE: Filled out chatbot screen with text

--- heart_beat_client/lib/screens/chatbot/chatbot_screen.dart
+++ heart_beat_client/lib/screens/chatbot/chatbot_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:heart_beat_client/widgets/chatbot/chat_bubble.dart';
 import 'package:heart_beat_client/widgets/common/bars/custom_app_bar.dart';
 import 'package:heart_beat_client/widgets/common/bars/custom_bottom_bar.dart';
+import 'package:heart_beat_client/widgets/stats/feedback_field.dart';
 
 class ChatbotScreen extends StatelessWidget {
   const ChatbotScreen({super.key});
@@ -10,7 +12,52 @@ class ChatbotScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Moody Blues"),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(
+          vertical: MediaQuery.of(context).size.height * 0.05,
+          horizontal: MediaQuery.of(context).size.width * 0.05,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: ChatBubble(text: "Hello", isUser: true),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ChatBubble(text: "Beep boop", isUser: false),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: null,
+                      decoration: InputDecoration(
+                        hintText: "What would you like to talk about...",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.04,
+                          vertical: MediaQuery.of(context).size.height * 0.04,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: CustomBottomBar(),
     );
   }
