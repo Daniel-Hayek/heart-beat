d4c934307467d1de5429fb39795fd4f998a74d0f
Danny <dhayek92@gmail.com>
2025-09-01 11:08:23 +0300

    FE: Filled out side bar and changed stylign

--- heart_beat_client/lib/widgets/common/bars/side_bar.dart
+++ heart_beat_client/lib/widgets/common/bars/side_bar.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heart_beat_client/core/constants/app_colors.dart';
+import 'package:heart_beat_client/widgets/common/buttons/secondary_button.dart';
 import 'package:heart_beat_client/widgets/common/fonts/title_text.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class SideBar extends StatelessWidget {
   const SideBar({super.key});
@@ -8,24 +11,49 @@ class SideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Colors.white,
-      child: ListView(
-        padding: EdgeInsets.zero,
+      backgroundColor: AppColors.secondaryColor,
+      child: Column(
         children: [
-          const DrawerHeader(
+          DrawerHeader(
             decoration: BoxDecoration(color: AppColors.primaryColor),
-            child: TitleText(text: "Menu", size: 20),
+            child: Container(
+              height: 100,
+              alignment: Alignment.center,
+              child: TitleText(text: "Menu", size: 24),
+            ),
           ),
           ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text("Home"),
+            leading: const Icon(LucideIcons.sheet, color: Colors.white),
+            title: const TitleText(text: "Mood Quizzes", size: 20),
             onTap: () {},
           ),
           ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text("Settings"),
+            leading: const Icon(
+              CupertinoIcons.profile_circled,
+              color: Colors.white,
+            ),
+            title: const TitleText(text: "Profile", size: 20),
             onTap: () {},
           ),
+          ListTile(
+            leading: const Icon(CupertinoIcons.gear, color: Colors.white),
+            title: const TitleText(text: "Settings", size: 20),
+            onTap: () {},
+          ),
+          Spacer(),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Text(
+              "Currently logged in as [EMAIL]",
+              style: TextStyle(
+                color: Colors.white.withValues(alpha: 0.7),
+                fontSize: 16,
+                fontFamily: 'montserrat',
+              ),
+            ),
+          ),
+          SecondaryButton(onPressed: () {}, label: "Logout"),
+          SizedBox(height: 100),
         ],
       ),
     );
