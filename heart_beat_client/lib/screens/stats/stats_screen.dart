c1c9edbe80b6a4bd161b9ca23eadbe17e1df7647
Danny <dhayek92@gmail.com>
2025-09-01 13:31:50 +0300

    FE: Removed a useless import that was causing Flutter CI to fail

--- heart_beat_client/lib/screens/stats/stats_screen.dart
+++ heart_beat_client/lib/screens/stats/stats_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heart_beat_client/core/constants/app_colors.dart';
 import 'package:heart_beat_client/routes/app_routes.dart';
