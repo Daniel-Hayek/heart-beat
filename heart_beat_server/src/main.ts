5ac0044dbd39f9e5ae86fb4e9a5fca07cbce5dd9
Danny <dhayek92@gmail.com>
2025-09-02 11:57:01 +0300

    BE: Added Swagger integration in main.ts

--- heart_beat_server/src/main.ts
+++ heart_beat_server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Heart-Beat APIs')
+    .setDescription(
+      'API Documentation for all available APIs on the Heart-Beat applcation',
+    )
+    .setVersion('1.0')
+    .addTag('Authentication')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
