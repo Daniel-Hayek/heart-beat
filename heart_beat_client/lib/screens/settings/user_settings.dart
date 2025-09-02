5c16a3ddf7de0a05283461f484a52e8ebdc19e27
Danny <dhayek92@gmail.com>
2025-09-02 11:04:40 +0300

    FE: Created basic checkbox widget to be used on settings page

--- heart_beat_client/lib/screens/settings/user_settings.dart
+++ heart_beat_client/lib/screens/settings/user_settings.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:heart_beat_client/widgets/common/bars/simple_app_bar.dart';
+import 'package:heart_beat_client/widgets/settings/settings_checkbox.dart';
 
-class UserSettings extends StatelessWidget {
+class UserSettings extends StatefulWidget {
   const UserSettings({super.key});
+
+  @override
+  State<UserSettings> createState() => _UserSettingsState();
+}
+
+class _UserSettingsState extends State<UserSettings> {
+  bool test = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SimpleAppBar(title: "Settings"),
-      body: Container(),
+      body: Column(
+        children: [
+          SettingsCheckbox(label: "Test", value: test, onChanged: (val) {
+            setState(() => test = val ?? false);
+          }),
+        ],
+      ),
     );
   }
 }
