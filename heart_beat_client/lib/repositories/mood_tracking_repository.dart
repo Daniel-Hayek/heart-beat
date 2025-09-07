475f8f59cf261a5f751bec3b9931fd59f4769339
Danny <dhayek92@gmail.com>
2025-09-07 20:10:37 +0300

    DAN-23 FE: Created createMoodEntry method in repository

--- heart_beat_client/lib/repositories/mood_tracking_repository.dart
+++ heart_beat_client/lib/repositories/mood_tracking_repository.dart
@@ -1,5 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:heart_beat_client/models/mood_tracking.dart';
 import 'package:heart_beat_client/services/api_service.dart';
 
 class MoodTrackingRepository {
   final _apiService = ApiService();
+
+  void createMoodEntry({
+    required String token,
+    required int userId,
+    required MoodTracking mood,
+  }) async {
+    try {
+      final response = await _apiService.client.post(
+        '/mood-tracking',
+        data: {
+          'userId': userId,
+          'source': mood.source,
+          'mood': mood.mood,
+          'score': mood.score,
+        },
+      );
+
+      if (response.statusCode == 201) {
+        debugPrint('Mood tracked successfully');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 }
