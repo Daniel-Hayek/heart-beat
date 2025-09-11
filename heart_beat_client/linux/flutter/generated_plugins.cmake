c22b08600d9213f47df4e3ad51724b061bb66cfa
Danny <dhayek92@gmail.com>
2025-09-11 09:57:23 +0300

    DAN-26 FE: Adjusted android build gradle to min sdk version 26 to support screen time plugin

--- heart_beat_client/linux/flutter/generated_plugins.cmake
+++ heart_beat_client/linux/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   flutter_secure_storage_linux
   gtk
+  screen_time
   url_launcher_linux
 )
 
--- heart_beat_client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ heart_beat_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import audio_session
 import flutter_secure_storage_macos
 import just_audio
 import path_provider_foundation
+import screen_time
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenTimePlugin.register(with: registry.registrar(forPlugin: "ScreenTimePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
