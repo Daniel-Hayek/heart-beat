bf8dda367fe6a7cf640a2dd37a1ef70525c39224
Danny <dhayek92@gmail.com>
2025-09-13 22:08:03 +0300

    DAN-33 FE: Created device data repository

---
+++ heart_beat_client/lib/repositories/device_data_repository.dart
@@ -0,0 +1,61 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:heart_beat_client/models/device_data.dart';
+import 'package:heart_beat_client/services/api_service.dart';
+
+class DeviceDataRepository {
+  final ApiService _apiService = ApiService();
+
+  Future<String> logData({
+    required int id,
+    required double sleepDuration,
+    required double activityLevel,
+    required double steps,
+    required double heartrate,
+    required int predictedStress,
+    required String token,
+  }) async {
+    try {
+      final response = await _apiService.client.post(
+        '/device-data',
+        data: {
+          'userId': id,
+          'sleep_duration': sleepDuration,
+          "heartrate": heartrate,
+          "activity_level": activityLevel,
+          "steps": steps,
+          "phone_usage": 230,
+        },
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      debugPrint(response.statusCode.toString());
+
+      return "Data logged successfully";
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<List<DeviceData>> fetchData({
+    required int userId,
+    required String token,
+  }) async {
+    try {
+      final response = await _apiService.client.get(
+        '/device-data/user/$userId',
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      final data = response.data as List;
+
+      debugPrint("=======================================");
+      debugPrint("Data fetched for $userId");
+      debugPrint("=======================================");
+
+      return data.map((json) => DeviceData.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception("Could not get user device data");
+    }
+  }
+}
